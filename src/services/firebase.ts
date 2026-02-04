006c472c5e20f7b8580641051da086f030877bfb
Jonkar5 <jonkarkodi@gmail.com>
2026-02-05 00:36:34 +0100

    Finalized production Firebase config

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -5,12 +5,13 @@ import { getStorage } from "firebase/storage";
 
 // TODO: Replace with your app's Firebase project configuration
 const firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID"
+  apiKey: "AIzaSyAL9qa10A-NYSeoKTqivVGaxB9UUOhrsaM",
+  authDomain: "dlkomaurrekontua.firebaseapp.com",
+  projectId: "dlkomaurrekontua",
+  storageBucket: "dlkomaurrekontua.firebasestorage.app",
+  messagingSenderId: "180428365229",
+  appId: "1:180428365229:web:9f726da5fa67a030df1524",
+  measurementId: "G-SDMKZP716L"
 };
 
 const app = initializeApp(firebaseConfig);
